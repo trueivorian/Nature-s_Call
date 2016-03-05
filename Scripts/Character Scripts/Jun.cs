da540147ad68d03fe0b009b379efc75819a90047
trueivorian <ocg14@ic.ac.uk>
2016-03-05 17:21:09 +0000

    Added Scripts Controlling Character Elements

---
+++ Scripts/Character Scripts/Jun.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jun : Character {
+
+	public static Jun junInstance;
+	private int level;
+	private enum weapon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void jump(){
+
+	}
+
+	public void attack(){
+
+	}
+
+	public void slide(){
+	
+	}
+
+	// public void moveLeft(){}
+	// public void moveRight(){}
+	// Cannot read other behaviour from the UML diagram
+
+}
---
+++ Scripts/Character Scripts/Plunger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plunger : Enemy {
+
+	public static Plunger plungerInstance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
